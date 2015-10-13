7ba380b77617a88a9371353121fcd931925d89be
Denys Gergun <gergund@gmail.com>
2015-10-13 20:41:19 +0300

    draft class

--- Lesson2/lesson2-1.php
+++ Lesson2/lesson2-1.php
@@ -5,3 +5,38 @@
  * Date: 27.09.15
  * Time: 13:01
  */
+
+class Node {
+
+    public $value;
+    public $parent;
+
+    public function __construct ($item){
+        $this->value = $item;
+        $this->parent = null;
+    }
+
+}
+
+class Tree {
+    protected $root;
+
+    public function __construct(){
+        $this->root=null;
+    }
+
+    public function isEmpty(){
+        return $this->root === null;
+    }
+
+    public function insert($item){
+        $node = new Node($item);
+
+        if ($this->isEmpty()) {
+            $this->root = null;
+        }
+        else {
+
+        }
+    }
+}
