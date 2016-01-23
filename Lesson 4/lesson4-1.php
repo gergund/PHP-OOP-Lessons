6691f41073e751ea0c16493f8acbbb7eada8469e
Denys Gergun <gergund@gmail.com>
2016-01-23 20:49:16 +0200

    Simple Interface example

---
+++ Lesson 4/lesson4-1.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 23.01.2016
+ * Time: 19:05
+ */
+
+interface Collector {
+
+    public function SetData( $data);
+    public function GetData();
+}
+
+class ArrayCollector implements Collector {
+
+    private $collector = array();
+
+    public function SetData( $data)
+    {
+        // TODO: Implement SetData() method.
+        $this->collector[] = $data;
+    }
+
+    public function GetData()
+    {
+        // TODO: Implement GetData() method.
+        return $this->collector;
+    }
+
+}
+
+$obj = new ArrayCollector();
+$obj->SetData(1);
+$obj->SetData(2);
+$obj->SetData(3);
+
+foreach ($obj->GetData() as $key=>$value) {
+    printf("Element[%s]: %s \n",$key,$value);
+}
