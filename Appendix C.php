682bfd283a67ed9e5717add70281f902d9ca70e4
Denys Gergun <gergund@gmail.com>
2016-01-31 17:31:07 +0200

    Example of permutation with repetition

--- Appendix C.php
+++ Appendix C.php
@@ -6,25 +6,30 @@
  * Time: 17:06
  */
 
-function getPermutations($str = '', $max = 0, $depth = 0, $row = array()) {
+function getPermutations($key = '', $max = 0, $depth = 0, $row = array()) {
     $perms = array();
     for($i = 0; $i <= $max; ++$i) {
         $row[$depth] = isset($row[$depth]) ? ($row[$depth] + 1) : 0;
         if($depth < $max) {
-            $res = getPermutations($str, $max, $depth + 1, $row);
+            $res = getPermutations($key, $max, $depth + 1, $row);
             foreach($res as $subrow)
                 $perms[] = $subrow;
         } else {
             $subrow = '';
             foreach($row as $char)
-                $subrow .= $str{$char};
+                $subrow .= $key{$char};
             $perms[] = $subrow;
         }
     }
     return($perms);
 }
-$str = 'abc';
+$key = 'abc';
+$alphabet = getPermutations($key, strlen($key) - 1);
 
-$alphabet = getPermutations($str, strlen($str) - 1);
+$index=0;
+for($i=65;$i<92;$i++){
+    $map[chr($i)] = $alphabet[$index];
+    $index++;
+}
 
-print_r($alphabet);
+print_r($map);
